6d3b3c3c0afffa591e44fef2c75e4128f6830f87
Thai Le Phan Minh <thailephanminh@gmail.com>
2022-01-12 20:56:43 +0700

    first commit

---
+++ service-worker.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = "version-1";
+
+const urlsToCache = [
+    "/",
+    "static/fonts/Roboto-Bold.ttf",
+    "static/fonts/Roboto-Regular.ttf",
+    "static/fonts/Roboto-Medium.ttf",
+    "static/fonts/Roboto-Light.ttf",
+    "static/css/base.css",
+    "static/css/main.4bafe407.css",
+    "static/js/main.6ea5934b.js",
+    "logo192.png",
+    "favicon.ico",
+];
+
+const self = this;
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("[Service worker] Cache installed");
+            return cache.addAll(urlsToCache);
+        }),
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    console.log("[Service worker] Cache fetched");
+
+    event.respondWith(
+        caches.match(event.request).then(function (cachedResponse) {
+            return (
+                cachedResponse ||
+                fetch(event.request).then(function (res) {
+                    caches
+                        .open(CACHE_NAME)
+                        .then((cache) => cache.put(event.request, res))
+                        // .catch(() => caches.match("index.html"));
+                    return res;
+                })
+            );
+        }),
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    const cacheWhiteList = [];
+    cacheWhiteList.push(CACHE_NAME);
+
+    console.log("[Service worker] Cache activated");
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>
+            Promise.all(
+                cacheNames
+                    .filter(function (cacheName) {
+                        return !cacheWhiteList.includes(cacheName);
+                    })
+                    .map(function (cacheName) {
+                        return caches.delete(cacheName);
+                    }),
+            ),
+        ),
+    );
+});
